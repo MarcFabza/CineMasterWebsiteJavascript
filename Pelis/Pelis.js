1903e9f9e0c651edb2828cd8b42584e5322f7290
MarcFabza <80487000+MarcFabza@users.noreply.github.com>
2025-01-10 22:38:16 +0100

    Add files via upload

---
+++ Pelis/Pelis.js
@@ -0,0 +1,117 @@
+const pelis=[
+    {
+        "titol" : "Jurassic Park",
+        "titolOriginal" : "Títol Original: Jurassic Park",
+        "anyPeli" : "Any: 1993",
+        "durada" : "Durada: 121 min",
+        "pais" : "País: Estats Units",
+        "bandera" : "../Imatges/Banderes/us.webp",
+        "director" : "Direcció: Steven Spilberg",
+        "generes" : "Gèneres: Aventura",
+        "imatge" : "../Imatges/JurassicPark.jpg",
+        "sinopsi" : "Sinopsi: El multimilionari John Hammond aconsegueix fer realitat el seu somni de clonar dinosaures del Juràssic i crear amb ells un parc temàtic en una illa remota. Abans d’obrir-lo al públic, convida una parella d’eminent científics i un matemàtic perquè comprovin la viabilitat del projecte. Però les mesures de seguretat del parc no preveuen l’instint de supervivència de la mare natura ni la cobdícia humana.",
+        "trailer" : "https://www.youtube.com/embed/lc0UehYemQA?si=1GDiEG3bobHJHhen",
+        "galeria" : ["../Imatges/Galeria/JurassicPark/jurassic_park-338513233-large.jpg","../Imatges/Galeria/JurassicPark/jurassic_park-559092143-large.jpg","../Imatges/Galeria/JurassicPark/jurassic_park-732545422-large.jpg","../Imatges/Galeria/JurassicPark/jurassic_park-821303145-large.jpg","../Imatges/Galeria/JurassicPark/jurassic_park-942364435-large.jpg","../Imatges/Galeria/JurassicPark/jurassic_park-961059308-large.jpg","../Imatges/Galeria/JurassicPark/jurassic_park-228735744-large.jpg","../Imatges/Galeria/JurassicPark/jurassic_park-884499021-large.jpg"]
+    },
+    {
+        "titol" : "Harry Potter i la Pedra Filosofal",
+        "titolOriginal" : "Títol Original: Harry Potter and the Sorcerer's Stone",
+        "anyPeli" : "Any: 2001",
+        "durada" : "Durada: 152 min",
+        "pais" : "País: Regne Unit",
+        "bandera" : "../Imatges/Banderes/gb.webp",
+        "director" : "Direcció: Chris Columbus",
+        "generes" : "Gèneres: Aventura, Fantasia",
+        "imatge" : "../Imatges/HarryPotter.jpg",
+        "sinopsi" : "Sinopsi: El dia que fa onze anys, Harry Potter s'assabenta que és fill de dos destacats mags, dels quals ha heretat poders màgics. A l'escola Hogwarts de Màgia, on s'educa amb altres nens que també tenen poders especials, aprendrà tot el necessari per ser mag.",
+        "trailer" : "https://www.youtube.com/embed/VyHV0BRtdxo?si=lBGE-jgbZ56TpeSv",
+        "galeria" : ["../Imatges/Galeria/HarryPotter/harry_potter_and_the_sorcerer_s_stone-276908495-large.PNG","../Imatges/Galeria/HarryPotter/harry_potter_and_the_sorcerer_s_stone-403373550-large.jpg","../Imatges/Galeria/HarryPotter/harry_potter_and_the_sorcerer_s_stone-493001671-large.jpg","../Imatges/Galeria/HarryPotter/harry_potter_and_the_sorcerer_s_stone-507474770-large.jpg","../Imatges/Galeria/HarryPotter/harry_potter_and_the_sorcerer_s_stone-683429125-large.jpg","../Imatges/Galeria/HarryPotter/harry_potter_and_the_sorcerer_s_stone-727123084-large.jpg","../Imatges/Galeria/HarryPotter/harry_potter_and_the_sorcerer_s_stone-782756584-large.jpg","../Imatges/Galeria/HarryPotter/harry_potter_and_the_sorcerer_s_stone-952491018-large.jpg"]
+    },
+    {
+        "titol" : "Pirates del Carib: La maledicció de la Perla Negra",
+        "titolOriginal" : "Títol Original: Pirates of the Caribbean: The Curse of the Black Pearl",
+        "anyPeli" : "Any: 2003",
+        "durada" : "Durada: 143 min",
+        "pais" : "País: Estats Units",
+        "bandera" : "../Imatges/Banderes/us.webp",
+        "director" : "Direcció: Gore Verbinski",
+        "generes" : "Gèneres: Aventura, Fantasia, Comedia, Acció",
+        "imatge" : "../Imatges/PiratasCaribe.jpg",
+        "sinopsi" : "Sinopsi: L'aventurer capità Jack Sparrow pirateja en aigües caribenyes, però les seves aventures acaben quan el seu enemic, el Capità Barbossa, després de robar-li el vaixell, la Perla Negra, ataca la ciutat de Port Royal i segresta Elizabeth Swann, la filla del Governador. Will Turner, amic de la infància d'Elizabeth, s'uneix a Jack per rescatar-la i recuperar el Perla Negra. Però Barbossa i la seva tripulació són víctimes d'un conjur que els condemna a viure eternament ia transformar-se cada nit en esquelets vivents. El conjur només es pot trencar si tornen una peça d'or asteca i salden un deute de sang. El rescat de la bella Elizabeth serà una tasca difícil, ja que la maledicció és real i serà difícil enfrontar-se amb els que no poden morir.",
+        "trailer" : "https://www.youtube.com/embed/naQr0uTrH_s?si=Ip_ce2Iyx8MkN9T5",
+        "galeria" : ["../Imatges/Galeria/PiratesOfTheCaribbean/pirates_of_the_caribbean_the_curse_of_the_black_pearl-148157856-large.jpg","../Imatges/Galeria/PiratesOfTheCaribbean/pirates_of_the_caribbean_the_curse_of_the_black_pearl-280984801-large.jpg","../Imatges/Galeria/PiratesOfTheCaribbean/pirates_of_the_caribbean_the_curse_of_the_black_pearl-289610994-large.jpg","../Imatges/Galeria/PiratesOfTheCaribbean/pirates_of_the_caribbean_the_curse_of_the_black_pearl-293602642-large.jpg","../Imatges/Galeria/PiratesOfTheCaribbean/pirates_of_the_caribbean_the_curse_of_the_black_pearl-483509506-large.jpg","../Imatges/Galeria/PiratesOfTheCaribbean/pirates_of_the_caribbean_the_curse_of_the_black_pearl-505668598-large.jpg","../Imatges/Galeria/PiratesOfTheCaribbean/pirates_of_the_caribbean_the_curse_of_the_black_pearl-752278362-large.jpg","../Imatges/Galeria/PiratesOfTheCaribbean/pirates_of_the_caribbean_the_curse_of_the_black_pearl-792857375-large.jpg"]
+    },
+    {
+        "titol" : "Spider-Man",
+        "titolOriginal" : "Títol Original: Spider-Man",
+        "anyPeli" : "Any: 2002",
+        "durada" : "Durada: 121 min",
+        "pais" : "País: Estats Units",
+        "bandera" : "../Imatges/Banderes/us.webp",
+        "director" : "Direcció: Sam Raimi",
+        "generes" : "Gèneres: Aventura, Fantasia, Superherois",
+        "imatge" : "../Imatges/Spiderman.jpg",
+        "sinopsi" : "Sinopsi: Després de la mort dels seus pares, Peter Parker, un tímid estudiant, viu amb la seva tieta May i el seu oncle Ben. No és un noi gaire popular a l'institut. Un dia li mossega una aranya que ha estat modificada genèticament; l'endemà al matí, descobreix estupefacte que posseeix la força i l'agilitat d'aquest insecte. Les aventures de l'home aranya es basen en el famós còmic de Stan Lee i Steve Ditko.",
+        "trailer" : "https://www.youtube.com/embed/t06RUxPbp_c?si=fHcsC8ELBW8UX2Fb",
+        "galeria" : ["../Imatges/Galeria/Spiderman/spider_man-400499939-large.jpg","../Imatges/Galeria/Spiderman/spider_man-534668679-large.jpg","../Imatges/Galeria/Spiderman/spider_man-598799450-large.jpg","../Imatges/Galeria/Spiderman/spider_man-660842744-large.jpg","../Imatges/Galeria/Spiderman/spider_man-704579327-large.jpg","../Imatges/Galeria/Spiderman/spider_man-817892980-large.jpg","../Imatges/Galeria/Spiderman/spider_man-858809999-large.jpg","../Imatges/Galeria/Spiderman/spider_man-982144000-large.jpg"]
+    },
+    {
+        "titol" : "El Hòbbit: Un Viatge Inesperat",
+        "titolOriginal" : "Títol Original: The Hobbit: An Unexpected Journey",
+        "anyPeli" : "Any: 2012",
+        "durada" : "Durada: 170 min",
+        "pais" : "País: Nova Zelanda",
+        "bandera" : "../Imatges/Banderes/nz.webp",
+        "director" : "Direcció: Peter Jackson",
+        "generes" : "Gèneres: Aventura, Fantasia, Acció",
+        "imatge" : "../Imatges/TheHobbit.jpg",
+        "sinopsi" : "Sinopsi: En companyia del mag Gandalf i de tretze nans, el hòbbit Bilbo Bolsón emprèn un viatge a través del país dels elfs i els boscos dels trolls, des de les masmorres dels orcs fins a la Muntanya Solitària, on el drac Smaug escombra el tresor de els Nans. Finalment, a les profunditats de la Terra, troba l'Anell Únic, hipnòtic objecte que serà posteriorment a causa de tantes sagnants batalles a la Terra Mitja.",
+        "trailer" : "https://www.youtube.com/embed/9PSXjr1gbjc?si=oEbHe6MqMxFxyLlY",
+        "galeria" : ["../Imatges/Galeria/TheHobbit/the_hobbit_an_unexpected_journey-398549660-large.jpg","../Imatges/Galeria/TheHobbit/the_hobbit_an_unexpected_journey-431293786-large.jpg","../Imatges/Galeria/TheHobbit/the_hobbit_an_unexpected_journey-463447301-large.jpg","../Imatges/Galeria/TheHobbit/the_hobbit_an_unexpected_journey-796766971-large.jpg","../Imatges/Galeria/TheHobbit/the_hobbit_an_unexpected_journey-826411382-large.jpg","../Imatges/Galeria/TheHobbit/the_hobbit_an_unexpected_journey-857014260-large.jpg","../Imatges/Galeria/TheHobbit/the_hobbit_an_unexpected_journey-899892306-large.jpg","../Imatges/Galeria/TheHobbit/the_hobbit_an_unexpected_journey-968791803-large.jpg"]
+    },
+    {
+        "titol" : "La Guerra de les Galàxies: L'Amenaça Fantasma",
+        "titolOriginal" : "Títol Original: Star Wars: The Phantom Menace",
+        "anyPeli" : "Any: 1999",
+        "durada" : "Durada: 130 min",
+        "pais" : "País: Estats Units",
+        "bandera" : "../Imatges/Banderes/us.webp",
+        "director" : "Direcció: George Lucas",
+        "generes" : "Gèneres: Aventura",
+        "imatge" : "../Imatges/StarWars.jpg",
+        "sinopsi" : "Sinopsi: La Federació de Comerç ha bloquejat el petit planeta de Naboo, governat per la jove Reina Amidala; es tracta d'un pla ideat per Sith Darth Sidious, que, mantenint-se a l'anonimat, dirigeix ​​els neimoidians, que estan al comandament de la Federació. El Jedi Qui-Gon Jinn i el seu aprenent Obi-Wan Kenobi convencen Amidala perquè vagi a Coruscant, la capital de la República i seu del Consell Jedi, i tracti de neutralitzar aquesta amenaça. Però, en intentar esquivar el bloqueig, la nau real resulta avariada, veient-se així obligada la tripulació a aterrar al desèrtic i remot planeta de Tatooine.",
+        "trailer" : "https://www.youtube.com/embed/bD7bpG-zDJQ?si=kzfJgJ07y-Qq8l5G",
+        "galeria" : ["../Imatges/Galeria/StarWars/star_wars_episode_i_the_phantom_menace-222553398-large.jpg","../Imatges/Galeria/StarWars/star_wars_episode_i_the_phantom_menace-323861331-large.jpg","../Imatges/Galeria/StarWars/star_wars_episode_i_the_phantom_menace-526777112-large.jpg","../Imatges/Galeria/StarWars/star_wars_episode_i_the_phantom_menace-626417316-large.jpg","../Imatges/Galeria/StarWars/star_wars_episode_i_the_phantom_menace-665379899-large.jpg","../Imatges/Galeria/StarWars/star_wars_episode_i_the_phantom_menace-688754371-large.jpg","../Imatges/Galeria/StarWars/star_wars_episode_i_the_phantom_menace-760719558-large.jpg","../Imatges/Galeria/StarWars/star_wars_episode_i_the_phantom_menace-839541710-large.jpg"]
+    }
+
+]
+
+function omplirDetalls(pelicula){
+    document.querySelector(".titol").innerHTML=pelicula.titol;
+    document.querySelector(".titolOriginal").innerHTML=pelicula.titolOriginal;
+    document.querySelector(".anyPeli").innerHTML=pelicula.anyPeli;
+    document.querySelector(".durada").innerHTML=pelicula.durada;
+    document.querySelector(".pais").innerHTML=pelicula.pais;
+    document.querySelector(".bandera img").src=pelicula.bandera;
+    document.querySelector(".director").innerHTML=pelicula.director;
+    document.querySelector(".generes").innerHTML=pelicula.generes;
+    document.querySelector(".sinopsi").innerHTML=pelicula.sinopsi;
+
+    document.querySelector(".imatge img").src=pelicula.imatge;
+
+    const trailerPelicula=document.querySelector(".trailer");
+    trailerPelicula.innerHTML=`<iframe width="560" height="315" src="${pelicula.trailer}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+
+    const galeriaPelicula=document.querySelector(".galeria ul");
+    galeriaPelicula.innerHTML="";
+    for(let i=0;i<pelicula.galeria.length;i=i+1){
+        const imatge=pelicula.galeria[i];
+        galeriaPelicula.innerHTML=galeriaPelicula.innerHTML+`<li><img src="${imatge}"></li>`
+    }
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const hash=window.location.hash;
+    const idPelicula=parseInt(hash.replace("#id=",""));
+    omplirDetalls(pelis[idPelicula]);
+})
